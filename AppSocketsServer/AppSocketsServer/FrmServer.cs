e0f6fecacf68f0dc5626c1256a093528d5c56ea8
Hiroshi <ebruno.hiroshi@gmail.com>
2023-07-22 14:12:38 -0500

    feat: login and send msg logic. add ClassComunica and ClassGeneral

--- AppSocketsServer/AppSocketsServer/FrmServer.cs
+++ AppSocketsServer/AppSocketsServer/FrmServer.cs
@@ -7,14 +7,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//Añadidos
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
 
 namespace AppSocketsServer
 {
     public partial class Server : Form
     {
+        private ClassGeneral gobernador = ClassGeneral.Instancia;
+        IPEndPoint ipEndPoint;
+        Socket socketPadre;
         public Server()
         {
             InitializeComponent();
+        }
+
+        public void createServer()
+        {
+            // establish the local end point for the socket 
+            IPHostEntry ipHost = Dns.Resolve("192.168.7.188");
+            IPAddress ipAddr = ipHost.AddressList[0];
+
+            ipEndPoint = new IPEndPoint(ipAddr, 13000); //colocar IPAdress.Any para 0.0.0.0
+
+            // create a Tcp/Ip Socket 
+            socketPadre = new Socket(ipEndPoint.AddressFamily,
+                                          SocketType.Stream, ProtocolType.Tcp);
+
+            Thread listener = new Thread(listen);
+            listener.Start();
+
+            // Start listening for connections
+        }
+
+        public void listen()
+        {
+            socketPadre.Bind(ipEndPoint);
+            socketPadre.Listen(10);
+            // Start listening for connections 
+            while (true)
+            {
+                try
+                {
+                    ClassComunica objeto = new ClassComunica();
+                    Console.WriteLine("Waiting for a connection on port {0}", ipEndPoint);
+
+                    // program is suspended while waiting for an incoming connection 
+                    objeto.socketComunica = socketPadre.Accept();
+                    string received = objeto.socketComunica.RemoteEndPoint.ToString();
+                    objeto.recibirHilo(received);
+                    //if (gobernador.usuariosConectados.Count < 6)
+                    //{
+                    //    gobernador.usuariosConectados.Add(objeto);
+                    //    //throw new Exception("Conexiones mayores a limite (10)");
+                    //}
+
+                    //this.Invoke(mostrarForm, received);
+                }
+                catch (Exception e)
+                {
+                    
+                }
+            }
+
         }
     }
 }
