533bebaf60b2843f1de5db41b0e800445f8d09f7
JohannCastillo <johannjco15022@gmail.com>
2023-07-22 11:38:38 -0500

    add: chat layout

--- AppSocketsClient/AppSocketsClient/Forms/FrmChat.Designer.cs
+++ AppSocketsClient/AppSocketsClient/Forms/FrmChat.Designer.cs
@@ -28,14 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlContacts = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnlChat = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnlFriend = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // pnlContacts
+            // 
+            this.pnlContacts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.pnlContacts.Location = new System.Drawing.Point(-5, 56);
+            this.pnlContacts.Name = "pnlContacts";
+            this.pnlContacts.Size = new System.Drawing.Size(302, 604);
+            this.pnlContacts.TabIndex = 0;
+            // 
+            // pnlChat
+            // 
+            this.pnlChat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(37)))));
+            this.pnlChat.Location = new System.Drawing.Point(294, 107);
+            this.pnlChat.Name = "pnlChat";
+            this.pnlChat.Size = new System.Drawing.Size(714, 553);
+            this.pnlChat.TabIndex = 1;
+            // 
+            // pnlFriend
+            // 
+            this.pnlFriend.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(23)))), ((int)(((byte)(28)))));
+            this.pnlFriend.Location = new System.Drawing.Point(294, 56);
+            this.pnlFriend.Name = "pnlFriend";
+            this.pnlFriend.Size = new System.Drawing.Size(714, 52);
+            this.pnlFriend.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.panel1.Location = new System.Drawing.Point(-5, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1013, 56);
+            this.panel1.TabIndex = 3;
             // 
             // FrmChat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(30)))), ((int)(((byte)(36)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1008, 659);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlFriend);
+            this.Controls.Add(this.pnlChat);
+            this.Controls.Add(this.pnlContacts);
             this.Name = "FrmChat";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmChat";
@@ -44,5 +84,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel pnlContacts;
+        private System.Windows.Forms.FlowLayoutPanel pnlChat;
+        private System.Windows.Forms.Panel pnlFriend;
+        private System.Windows.Forms.Panel panel1;
     }
 }
