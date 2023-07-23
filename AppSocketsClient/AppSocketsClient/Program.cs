3ae2994953d160e8ac97d671af0cc29149ee78bb
brunomont <brunoadrian.mont@gmail.com>
2023-07-23 19:06:11 +0200

    run login first

--- AppSocketsClient/AppSocketsClient/Program.cs
+++ AppSocketsClient/AppSocketsClient/Program.cs
@@ -16,8 +16,8 @@ namespace AppSocketsClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Forms.FrmLogin());
-            Application.Run(new Forms.FrmChat());
+            Application.Run(new Forms.FrmLogin());
+            // Application.Run(new Forms.FrmChat());
         }
     }
 }
