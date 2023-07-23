84c13fe32d4e366498bbba339a3b76b64c0bd9e4
brunomont <brunoadrian.mont@gmail.com>
2023-07-23 20:31:29 +0200

    feat: add usersession class

---
+++ AppSocketsClient/AppSocketsClient/Helpers/UserSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSocketsClient.Helpers
+{
+    public class UserSession
+    {
+        public string Username { get; set; }
+        public List<string> OnlineUsers { get; set; }
+
+        public UserSession(string username, string[] onlineusers = null)
+        {
+            Username = username;
+            OnlineUsers = onlineusers != null ? new List<string>(onlineusers) : new List<string>();
+        }
+    }
+}
